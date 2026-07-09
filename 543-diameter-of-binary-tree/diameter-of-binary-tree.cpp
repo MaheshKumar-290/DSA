53e1b4da1bd9e422e45a1de49b431a2e4d8e16a8
Mahesh Kumar <mkumar_be24@thapar.edu>
2026-07-10 00:39:48 +0530

    Time: 264 ms (15.93%) | Memory: 23.9 MB (11.85%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,23 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    int res = 0;
-
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        dfs(root);
-        return res;        
+int height(TreeNode* root){
+    if(root==nullptr){
+        return 0;
     }
 
-private:
-    int dfs(TreeNode* root) {
-        if (!root) {
+int left=height(root->left);
+int right=height(root->right);
+
+
+return max(left,right)+1;
+
+
+}
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==nullptr){
             return 0;
         }
 
-        int l = dfs(root->left);
-        int r = dfs(root->right);
+int l_max=0;
+int r_max=0;
+int ans=0;
+// int h=height(root);
 
-        res = std::max(res, l + r);
 
-        return 1 + std::max(l, r);
-    }    
+int left =height(root->left);
+int right=height(root->right);
+
+if(      left+right >ans){
+    ans =left+right;
+
+}
+
+// else if(  root->left==nullptr && root->right!=nullptr &&    left+right +1 >ans){
+//     ans =left+right+1;
+
+// }
+// else if(  root->left!=nullptr && root->right==nullptr &&    left+right +1 >ans){
+//     ans =left+right+1;
+
+// }
+
+
+
+l_max=diameterOfBinaryTree(root->left);
+r_max=diameterOfBinaryTree(root->right);
+
+
+
+return max(max(l_max,r_max),ans);
+        
+    }
 };
