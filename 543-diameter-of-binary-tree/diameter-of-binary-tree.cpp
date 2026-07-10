0d11240e48e422709c30085013985d681872cd6a
Mahesh Kumar <mkumar_be24@thapar.edu>
2026-07-10 11:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (42.44%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+int ans=0;
 int height(TreeNode* root){
     if(root==nullptr){
         return 0;
@@ -18,6 +19,9 @@ int height(TreeNode* root){
 
 int left=height(root->left);
 int right=height(root->right);
+int check=left+right;
+
+ans=max(check,ans);
 
 
 return max(left,right)+1;
@@ -26,41 +30,16 @@ return max(left,right)+1;
 }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root==nullptr){
-            return 0;
-        }
+      if(root==nullptr){
+        return 0;
+    }
 
-int l_max=0;
-int r_max=0;
-int ans=0;
-// int h=height(root);
-
-
-int left =height(root->left);
-int right=height(root->right);
-
-if(      left+right >ans){
-    ans =left+right;
-
-}
-
-// else if(  root->left==nullptr && root->right!=nullptr &&    left+right +1 >ans){
-//     ans =left+right+1;
-
-// }
-// else if(  root->left!=nullptr && root->right==nullptr &&    left+right +1 >ans){
-//     ans =left+right+1;
-
-// }
+int left=height(root);
 
 
 
-l_max=diameterOfBinaryTree(root->left);
-r_max=diameterOfBinaryTree(root->right);
 
-
-
-return max(max(l_max,r_max),ans);
+return ans;
         
     }
 };
