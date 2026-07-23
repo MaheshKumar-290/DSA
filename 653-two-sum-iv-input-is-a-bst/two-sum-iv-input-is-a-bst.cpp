37adddea6118a6e65d847da8ee395a1a6d107a0f
Mahesh Kumar <mkumar_be24@thapar.edu>
2026-07-23 21:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 36.5 MB (97.96%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,11 +1,88 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    unordered_set<int> set;
-
 public:
-    bool findTarget(TreeNode* root, int k) {
-        if (!root) return false;
-        if (set.count(k - root->val)) return true;
-        set.insert(root->val);
-        return findTarget(root->left, k) || findTarget(root->right, k);
+bool find(TreeNode* root,int t){
+bool left=false;
+bool right=false;
+
+
+if(root==nullptr){
+    return false;
+}
+
+if(root->val==t){
+    return true;
+}
+
+else if(root->val>t){
+   left= find(root->left,t);
+}
+
+if(left){
+    return true;
+}
+
+else if(root->val<t){
+  right = find(root->right,t);
+}
+
+
+return right;
+
+}
+
+
+    bool FindTarget(TreeNode* root, int k,TreeNode* parent) {
+
+
+        if(root==nullptr){
+            return false;
+        }
+
+
+if(k==2*root->val){
+    
+}
+  else  if(find(parent,k-root->val)){
+            return true ;
+        }
+
+
+        bool left=FindTarget(root->left,k,parent);
+
+if(left){
+    return true;
+}
+         bool right=FindTarget(root->right,k,parent);
+        if(left || right){
+            return true;
+        }
+        
+
+        return false;
     }
+
+
+
+    bool findTarget(TreeNode* root, int k) {
+
+
+        if(root==nullptr){
+            return false;
+        }
+
+return FindTarget(root,k,root);
+
+      
+}
 };
