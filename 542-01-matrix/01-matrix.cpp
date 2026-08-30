b8c4dbf3297f7517330091b8d0193933077a810f
Mahesh Kumar <mkumar_be24@thapar.edu>
2026-08-30 14:56:49 +0530

    Time: 36 ms (11.40%) | Memory: 39.3 MB (9.81%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,106 @@
+class Solution {
+
+
+
+
+public:
+
+
+
+
+void bsf( int i,int j ,vector<vector<int>>& visited,vector<vector<int>>& mat){
+
+ int n=mat.size();
+        int m=mat[0].size();
+int row[]={0,-1,0,1};
+int col[]={1,0,-1,0};
+
+
+        queue<pair<int,int>>qu;
+        qu.push({i,j});
+
+        while(!qu.empty()){
+            auto node =qu.front();
+            qu.pop();
+
+            for(int x=0;x<4;x++){
+                int r=row[x]+node.first;
+                int c=col[x]+node.second;
+
+
+                if(r>-1 && r<n && c<m && c>-1 ){
+                 if(visited[r][c]==-1)  { qu.push({r,c});}
+
+                        if(mat[r][c]>mat[node.first][node.second]+1    || (visited[r][c]==-1 && mat[r][c]==1)  ){
+                            mat[r][c]=mat[node.first][node.second]+1;
+                            qu.push({r,c});
+
+
+                        }
+
+
+
+                        visited[r][c]=1;
+
+
+
+
+                }
+
+
+
+            }
+
+            
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+
+
+
+
+
+
+
+
+
+         vector<vector<int>>visited(n,vector<int>(m,-1));
+
+         for( int i=0;i<n;i++){
+
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0  && visited[i][j]==-1){
+                    bsf(i,j,visited,mat);
+                }
+
+
+
+
+
+            }
+         }
+         return mat;
+    }
+};
