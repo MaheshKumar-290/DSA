2ab9d891c7ebccff2eecd5296035e9b3378e4b58
Mahesh Kumar <mkumar_be24@thapar.edu>
2026-09-04 22:44:49 +0530

    Time: 1513 ms (5.01%) | Memory: 19 MB (97.36%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+
+
+bool check(string a, string b){
+
+
+int size=a.size();
+int flag=0;
+
+for(int i=0;i<size;i++){
+
+if(a[i]!=b[i]){
+    flag++;
+
+    if(flag>1){
+        return false;
+    }
+}
+
+
+
+}
+
+
+
+if(flag==1){
+return true;}
+
+return false;
+}
+
+
+
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+
+
+queue<pair<string,int> >qu;
+int n= wordList.size();
+vector<int>visited(n,-1);
+
+
+
+
+for(int i=0 ;i <n ;i++){
+
+    if(check(beginWord , wordList[i]) && visited[i]==-1){
+        qu.push({ wordList[i],1});
+        visited[i]=1;
+
+    }
+}
+
+while(!qu.empty()){
+
+auto temp=qu.front();
+qu.pop();
+
+if(temp.first==endWord){
+
+    return temp.second+1;
+}
+
+
+
+for(int i=0 ;i <n ;i++){
+
+    if(check(temp.first , wordList[i]) && visited[i]==-1){
+        qu.push({ wordList[i],temp.second+1});
+        visited[i]=1;
+
+    }
+}
+
+
+
+
+
+
+
+}
+
+
+return 0;
+        
+    }
+};
